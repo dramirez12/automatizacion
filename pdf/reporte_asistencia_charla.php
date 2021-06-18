65c3902a0c6c1045fb66256166f713f6ca58e778
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-06-18 13:04:10 -0600

    charla direccion
    redireccion de charla

--- pdf/reporte_asistencia_charla.php
+++ pdf/reporte_asistencia_charla.php
@@ -11,7 +11,7 @@ if(!empty($_GET)) {
 
 $sql = "SELECT DISTINCTROW px.valor, CONCAT(p.nombres,' ',p.apellidos) AS nombre, cp.no_constancia, cp.jornada as jornada 
 FROM tbl_personas p,tbl_personas_extendidas px, tbl_charla_practica cp 
-WHERE p.id_persona=cp.id_persona AND cp.jornada =$jornada AND px.id_persona=cp.id_persona 
+WHERE p.id_persona=cp.id_persona AND cp.jornada = $jornada AND px.id_persona=cp.id_persona 
 AND cp.estado_asistencia_charla = 1";
 
 class PDF extends FPDF
