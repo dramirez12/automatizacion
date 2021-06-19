aab721fdd9824605b1ce775b5530b3ecebc0c2f8
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-06-18 22:22:27 -0600

    red
    red

--- api/asistencia_charla_api.php
+++ api/asistencia_charla_api.php
@@ -68,7 +68,7 @@ elseif (isset($_GET["asistencia"])) {
 					$Id_objeto = 14;
 					bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'], 'ACTUALIZO', 'LA ASISTENCIA CHARLA.');
 					// echo "<script> window.location.replace('http://informaticaunah.com/automatizacion/vistas/gestion_asistencia_charla_vista.php?msj=2'); </script>";
-					
+					header("location:../vistas/gestion_asistencia_charla_vista.php?msj=2"); 
 					
 				} else {
 					//  header("location:../vistas/gestion_asistencia_charla_vista.php?msj=3"); 
