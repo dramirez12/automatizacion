62d62b50ae0b9a6f28e70e1978e31963562a5546
Gabriel Obando <geobjaja@hotmail.com>
2021-06-21 14:31:41 -0600

    FyS

--- pdf/reporte_constancia_charla.php
+++ pdf/reporte_constancia_charla.php
@@ -23,7 +23,7 @@ class PDF extends FPDF
 		$this->Image('../dist/img/logo_ia.jpg', 15,8,37);
 			$this->Ln(5);
 						$this->Image('../dist/img/logo-unah.jpg', 240, 8, 25 );
-						$this->Image('../dist/img/Firmaysello.jpg', 110, 160, 120 );
+						$this->Image('../dist/img/Firmaysello.jpg', 100, 140, 110 );
 		}
 function Footer()
 		{
