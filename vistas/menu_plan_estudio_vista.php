00947c358a856d43f657ef8eb296b6b9c8815245
fany121 <61065845+fany121@users.noreply.github.com>
2021-06-23 14:29:02 -0600

    c

--- vistas/menu_plan_estudio_vista.php
+++ vistas/menu_plan_estudio_vista.php
@@ -60,9 +60,8 @@
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     <div class="wrapper">
-        Content Wrapper. Contains page content 
-<div class="content-wrapper">
-    Content Header (Page header) 
+       
+<div class="content-wrapper"> 
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
