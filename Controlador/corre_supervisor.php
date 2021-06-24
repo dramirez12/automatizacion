970cf6e85fee819617ff96e6693d8445df1ee152
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-06-24 16:30:02 -0600

    puerto
    puerto correo

--- Controlador/corre_supervisor.php
+++ Controlador/corre_supervisor.php
@@ -22,7 +22,7 @@ class correo
 		$Password = "inforunah2021";
 		$mail->SMTPDebug = 0;
 		$mail->Host = 'smtp.gmail.com';
-		$mail->Port = 465;
+		$mail->Port = 993;
 		$mail->SMTPSecure = 'ssl';
 		$mail->SMTPAuth = true;
 		$mail->Username = $correo;
@@ -58,7 +58,7 @@ class correo
 		$Password = "inforunah2021";
 		$mail->SMTPDebug = 0;
 		$mail->Host = 'smtp.gmail.com';
-		$mail->Port = 465;
+		$mail->Port = 993;
 		$mail->SMTPSecure = "ssl";
 		$mail->SMTPAuth = true;
 		$mail->Username = $correo;
