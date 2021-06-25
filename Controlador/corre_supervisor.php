34b2e30eca35bc49a6e9cef2c9128eb6c388256c
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-06-25 12:48:32 -0600

    comentando if
    if del correo para probar

--- Controlador/corre_supervisor.php
+++ Controlador/corre_supervisor.php
@@ -4,41 +4,8 @@ require_once ('../PHPMAILER/src/SMTP.php');
 require_once ('../PHPMAILER/src/Exception.php');
 require_once ('../PHPMAILER/src/OAuth.php');
 require_once ('../clases/Conexion.php');
-
 class correo
 {
-
-	function enviarEmailDocente($cuerpo, $asunto_docente, $destino, $nombre_destino)
-	{
-
-		$mail = new PHPMailer\PHPMailer\PHPMailer();
-
-
-		$mail->isSMTP();
-
-		$correo_doc = "vinculacionunah@informaticaunah.com";
-		$Password_doc = "N5y*%U(Ofb+T";
-		$mail->SMTPDebug = 0;
-		$mail->Host = 'informaticaunah.com';
-		$mail->Port = 465;
-		$mail->SMTPSecure = 'ssl';
-		$mail->SMTPAuth = true;
-		$mail->Username = $correo_doc;
-		$mail->Password = $Password_doc;
-		$mail->setFrom($correo_doc, 'Unidad de Vinculación Departamento de Informática');
-		$mail->addAddress($destino, $nombre_destino);
-		$mail->Subject = $asunto_docente;
-		$mail->Body = $cuerpo;
-		$mail->CharSet = 'UTF-8';
-		$mail->IsHTML(true);
-
-		if (!$mail->send()) {
-			echo "Error al enviar el E-Mail: " . $mail->ErrorInfo;
-		} else {
-			echo "muy bien docente";
-			exit();
-		}
-	}
 
 	function enviarEmailPracticante($cuerpo_estudiante, $asunto_estudiante, $ecorreo, $estudiante)
 	{
@@ -65,18 +32,48 @@ class correo
 		$mail->CharSet = 'UTF-8';
 		$mail->IsHTML(true);
 
-		if (!$mail->send()) {
-			echo "Error al enviar el E-Mail: " . $mail->ErrorInfo;
-		} else {
-			echo "muy bien estudiante";
-			exit();
-		}
+		// if (!$mail->send()) {
+		// 	echo "Error al enviar el E-Mail: " . $mail->ErrorInfo;
+		// } else {
+		// 	echo "muy bien estudiante";
+		// 	exit();
+		// }
 		
 	} 
+
+	function enviarEmailDocente($cuerpo, $asunto_docente, $destino, $nombre_destino)
+	{
+
+		$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+
+		$mail->isSMTP();
+
+		$correo_doc = "vinculacionunah@informaticaunah.com";
+		$Password_doc = "N5y*%U(Ofb+T";
+		$mail->SMTPDebug = 0;
+		$mail->Host = 'informaticaunah.com';
+		$mail->Port = 465;
+		$mail->SMTPSecure = 'ssl';
+		$mail->SMTPAuth = true;
+		$mail->Username = $correo_doc;
+		$mail->Password = $Password_doc;
+		$mail->setFrom($correo_doc, 'Unidad de Vinculación Departamento de Informática');
+		$mail->addAddress($destino, $nombre_destino);
+		$mail->Subject = $asunto_docente;
+		$mail->Body = $cuerpo;
+		$mail->CharSet = 'UTF-8';
+		$mail->IsHTML(true);
+
+		// if (!$mail->send()) {
+		// 	echo "Error al enviar el E-Mail: " . $mail->ErrorInfo;
+		// } else {
+		// 	echo "muy bien docente";
+		// 	exit();
+		// }
+	}
 
 
 	
 
 }//cierre class
-
-
