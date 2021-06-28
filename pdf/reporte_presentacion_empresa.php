91ad888721d00b991c38f77ee52facb355d562c1
Gabriel Obando <geobjaja@hotmail.com>
2021-06-28 04:40:25 -0600

    Ortografia Carta de Presentacion

--- pdf/reporte_presentacion_empresa.php
+++ pdf/reporte_presentacion_empresa.php
@@ -44,11 +44,11 @@ $resultado = mysqli_query($connection, $sqltabla);
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
 	$pdf->SetFont('Arial','B',15);
-	$pdf->cell(0,6,utf8_decode('Universidad Nacional Autonoma de Honduras'),0,1,'C');
+	$pdf->cell(0,6,utf8_decode('Universidad Nacional Autónoma de Honduras'),0,1,'C');
 	$pdf->ln(2);
-	$pdf->cell(0,6,utf8_decode('Facultad de Ciencias Economicas'),0,1,'C');
+	$pdf->cell(0,6,utf8_decode('Facultad de Ciencias Económicas'),0,1,'C');
 	$pdf->ln(2);
-	$pdf->cell(0,6,utf8_decode('Departamento de Informatica Administrativa'),0,1,'C');
+	$pdf->cell(0,6,utf8_decode('Departamento de Informática Administrativa'),0,1,'C');
 	$pdf->SetFont('Arial','',12);
     $pdf->ln(5);
         $pdf->ln(5);
@@ -67,18 +67,18 @@ $resultado = mysqli_query($connection, $sqltabla);
     $pdf->multicell(170,9,utf8_decode('Estimado '),0);
 	$pdf->ln(5);
 	$pdf->SetX(20);
-	$pdf->multicell(170,5,utf8_decode('Por este medio me permito presentar a : '.$row['nombre'].'  con numero de cuenta '.$row['valor'].'  estudiante de la carrera de Informatica Administrativa, quien desea realizar la practica profecional en tan prestigiosa empresa. '),0);
+	$pdf->multicell(170,5,utf8_decode('Por este medio me permito presentar a : '.$row['nombre'].'  con número de cuenta '.$row['valor'].'  estudiante de la carrera de Informática Administrativa, quien desea realizar la práctica profesional en tan prestigiosa empresa. '),0);
 	$pdf->ln(5);
 	$pdf->SetX(20);
-	$pdf->multicell(170,5,utf8_decode('La Practica Profesional es una actividad formativa del alumno, consistente en la asuncion supervisada y gradual, del rol profesional, a traves de su insercion a un arealida o ambiente laboral especifico, al mismo tiempo se convierte en un aporte desde su capacidad,habilidad y conocimientos adquiridos, cuya meta es producir algun producto o aporte significativo dentro de la institucion. '),0);
+	$pdf->multicell(170,5,utf8_decode('La Práctica Profesional es una actividad formativa del alumno, consistente en la asunción supervisada y gradual, del rol profesional, a través de su inserción a una realidad o ambiente laboral específico, al mismo tiempo se convierte en un aporte desde su capacidad, habilidad y conocimientos adquiridos, cuya meta es producir algún producto o aporte significativo dentro de la institución. '),0);
 	$pdf->ln(5);
 	$pdf->SetX(20);
-    $pdf->multicell(170,5,utf8_decode('Este es un requisito de graduacion para los estudiantes de las carreras de la Facultad de Ciencias Economicas Administrativas y Contables, la cual tiene una duracion de 800 horas y puede ser realizada a partir de que los alumnos hayan cursado como minimo el 80% de las A signaturas del plan de estudios de su Carrera. Para continuar con los tramites relacionados con la practica profesional le solicitamos lo siguiente: '),0);
+    $pdf->multicell(170,5,utf8_decode('Este es un requisito de graduación para los estudiantes de las carreras de la Facultad de Ciencias Económicas Administrativas y Contables, la cual tiene una duración de 800 horas y puede ser realizada a partir de que los alumnos hayan cursado como minimo el 80% de las asignaturas del plan de estudios de su Carrera. Para continuar con los tramites relacionados con la práctica profesional le solicitamos lo siguiente: '),0);
 	$pdf->SetX(20);
 	$pdf->multicell(170,5,utf8_decode('
-		a.Perfil de la empresa: Mision, Vision, objetivos/metas y datos generales de los contactos de la empresa.
-		b.El detalle de las actividades que realizara el estudiante de acuerdo al perfil de la carrera de Informatica Adminstrativa que consiste en las catividades como: analisis, diseño, codificacion e implementacion de sistemas de informacion; diseño, manejo y adminstracion de bases de datos, redes y comunicacion de datos, soporte de aplicaciones, y otros relacionados con el area tecnologica.
-		Una vez recibida la documentacion solicitada, el comite de Practica Profesional de esta Carrera procedera autorizar al estudiante este requisito de graduacion, dependiendo del analisis de la informacion proporcionada.'),0);
+		a.Perfil de la empresa: Misión, Visión, objetivos/metas y datos generales de los contactos de la empresa.
+		b.El detalle de las actividades que realizará el estudiante de acuerdo al perfil de la carrera de Informática Adminstrativa que consiste en las actividades como: análisis, diseño, codificación e implementación de sistemas de información; diseño, manejo y adminstración de bases de datos, redes y comunicación de datos, soporte de aplicaciones, y otros relacionados con el area tecnológica.
+		Una vez recibida la documentación solicitada, el comité de Práctica Profesional de esta Carrera procederá a autorizar al estudiante este requisito de graduación, dependiendo del análisis de la información proporcionada.'),0);
 	$pdf->ln(5);
 	$pdf->SetX(20);
 	$pdf->multicell(170,5,utf8_decode('
