35e4ec96df03d8370a6bca3c24af3d24f0c94c53
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-07-05 17:31:26 -0600

    Revert "actualizar ser producc"
    This reverts commit e44ed9b9df51bd79225c29e8d6df5d6a3df86fdd.

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -2,27 +2,27 @@
 
 
 
-$servidor= "167.114.169.207";
-$usuario= "informat_informaticaunah2";
-$password = "dF%&(M6tJL?P";
-$base= "informat_automatizacion";
+    $servidor= "167.114.169.207";
+    $usuario= "informat_desarrollo";
+    $password = "!fuRCr3XR-tz";
+    $base= "informat_desarrollo_automatizacion";
 
-$mysqli = new mysqli($servidor, $usuario,$password,$base);
-$connection = mysqli_connect($servidor, $usuario,$password,$base) or die("Error " . mysqli_error($connection));
-
-if($mysqli->connect_error){
-    echo "Nuestro sitio presenta fallas....";
-    die('Error en la conexion' . $mysqli->connect_error);
-    exit();	
-}
-$connect = new PDO("mysql:host=167.114.169.207;dbname=informat_automatizacion", "informat_informaticaunah2", "dF%&(M6tJL?P");
+	$mysqli = new mysqli($servidor, $usuario,$password,$base);
+	$connection = mysqli_connect($servidor, $usuario,$password,$base) or die("Error " . mysqli_error($connection));
+	
+	if($mysqli->connect_error){
+		echo "Nuestro sitio presenta fallas....";
+		die('Error en la conexion' . $mysqli->connect_error);
+		exit();	
+	}
+ $connect = new PDO("mysql:host=167.114.169.207;dbname=informat_desarrollo_automatizacion", "informat_desarrollo", "!fuRCr3XR-tz");
 
 if (!mysqli_set_charset($mysqli, "utf8")) {
-    printf("Error cargando el conjunto de caracteres utf8: %s\n", mysqli_error($mysqli));
-    exit();
-} else {
-    printf("");
-}
+        printf("Error cargando el conjunto de caracteres utf8: %s\n", mysqli_error($mysqli));
+        exit();
+    } else {
+        printf("");
+    }
 
     
 	
