7f57896f457028c5093ab3c7187639b00f7db43e
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-07-07 16:29:45 -0600

    modelo APPS

---
+++ Controlador/aprobar_practica_controlador.php
@@ -0,0 +1,30 @@
+
+<?php
+session_start();
+require_once ("../Modelos/calculo_fecha_pps_modelos.php");
+
+$Id_objeto = 46;
+$MU = new modeloCarga();
+
+$cuenta_estud=$_POST['cuenta_estud'];
+$obs_prac=$_POST['obs_prac'];
+$empresa_prac=$_POST['empresa_prac'];
+$hrs_pps=$_POST['hrs_pps'];
+$fecha_inicio_prac=$_POST['fecha_inicio_prac'];
+$fecha_final_prac=$_POST['fecha_final_prac'];
+
+
+
+$rspta = $db->update_pps($cuenta_estud, $obs_prac, $empresa_prac, $hrs_pps, $fecha_inicio_prac, $fecha_final_prac);
+echo $rspta;
+
+// if ($consulta === 1) {
+//     bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'], 'INGRESÓ', 'UNA NUEVA CARGA ACADÉMICA');
+
+// } else {
+
+// }
+
+
+
+?>
