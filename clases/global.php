0a26990388ced22883c971f34f73f20475173fce
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-07-08 14:43:42 -0600

    CAMBIO DE CREDENCIALES PARA VIDEO TUTORIAL

--- clases/global.php
+++ clases/global.php
@@ -3,13 +3,13 @@
 define("DB_HOST","167.114.169.207");
 
 //Nombre de la base de datos
-define("DB_NAME", "informat_desarrollo_automatizacion");
+define("DB_NAME", "informat_automatizacion");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "informat_desarrollo");
+define("DB_USERNAME", "informat_informaticaunah2");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "!fuRCr3XR-tz");
+define("DB_PASSWORD", "dF%&(M6tJL?P");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
@@ -17,3 +17,6 @@ define("DB_ENCODE","utf8");
 //Definimos una constante como nombre del proyecto
 define("PRO_NOMBRE","ITVentas");
 ?>
+
+
+
