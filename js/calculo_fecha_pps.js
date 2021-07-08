04474c2a2236c0d50dbf76442a493e21e6735330
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-07-08 11:14:11 -0600

    APPS

--- js/calculo_fecha_pps.js
+++ js/calculo_fecha_pps.js
@@ -21,36 +21,7 @@ $(document).ready(function () {
 
     $('#btn_aprobacion_rechazo_practica').on('click', function () {
 
-        // var url = "../Controlador/calculo_fecha_pps_controlador.php?op=update";
-        // $.ajax({                        
-        //    type: "POST",                 
-        //    url: url,                     
-        //    data: $("#formulario").serialize(), 
-        //    success: function(data)             
-        //    {
-        //        if(data==0)
-        //        {
-        //         swal({
-        //             title: 'Práctica aprobada con éxito',
-        //             type: "success",
-        //             button: "OK",
-        //           }).then(function() {
-        //             location.href = '../vistas/aprobar_practica_coordinacion_vista.php';
-        //         }); 
-        //        }
-        //        else if(data==1)
-        //        {
-        //         swal({
-        //             title: 'Práctica no se pudo aprobar',
-        //             type: "error",
-        //             button: "OK",
-        //           }).then(function() {
-        //             location.href = '../vistas/aprobar_practica_coordinacion_vista.php';
-        //         }); 
-        //        }
-
-        //    }
-        // });
+        
 
         var cuenta_estudid = $("#txt_estudiante_cuenta").val();
         var obs = $("#txt_motivo_rechazo").val();
