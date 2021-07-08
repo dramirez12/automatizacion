68994ec705a7a39b99d17ed33399e6c3d0a2eaea
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-07-08 11:28:33 -0600

    APPS

--- js/calculo_fecha_pps.js
+++ js/calculo_fecha_pps.js
@@ -23,23 +23,23 @@ $(document).ready(function () {
 
         
 
-        var cuenta_estudid = $("#txt_estudiante_cuenta").val();
-        var obs = $("#txt_motivo_rechazo").val();
+        var cuenta_estud = $("#txt_estudiante_cuenta").val();
+        var obs_prac = $("#txt_motivo_rechazo").val();
         var empresa_prac = $("#txt_empresa").val();
         var hrs_pps = $("#cb_horas_practica").val();
-        var fecha_inicio = $("#fecha_inicio").val();
-        var fecha_final = $("#fecha_finalizacion").val();
+        var fecha_inicio_prac = $("#fecha_inicio").val();
+        var fecha_final_prac = $("#fecha_finalizacion").val();
 
         $.ajax({
             url: "../Controlador/aprobar_practica_controlador.php",
             type: "POST",
             data: {
-                cuenta_estud: cuenta_estudid,
-                obs_prac: obs,
+                cuenta_estud: cuenta_estud,
+                obs_prac: obs_prac,
                 empresa_prac: empresa_prac,
                 hrs_pps: hrs_pps,
-                fecha_inicio_prac: fecha_inicio,
-                fecha_final_prac: fecha_final,
+                fecha_inicio_prac: fecha_inicio_prac,
+                fecha_final_prac: fecha_final_prac,
             },
         }).done(function (resp) {
             if (resp > 0) {
