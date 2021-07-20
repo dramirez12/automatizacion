5c20bbeb4b3f8638ab079ac66afc97cbabd094be
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-07-20 13:09:08 -0600

    n

--- Controlador/aprobar_practica_controlador.php
+++ Controlador/aprobar_practica_controlador.php
@@ -51,131 +51,131 @@ $id_objeto = 21;
 // echo $rspta1;
 
 
-// $asunto_estudiante="APROBACIÓN DE PRÁCTICA PROFESIONAL SUPERVISADA";
+$asunto_estudiante_aproba ="APROBACIÓN DE PRÁCTICA PROFESIONAL SUPERVISADA";
 
-// $cuerpo = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-// <html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml">
-// <head>
-// <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-// <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-// <title>------------</title>
+$cuerpo_aproba = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>------------</title>
 
-// </head>
-// <body style="-webkit-text-size-adjust: none; box-sizing: border-box; color: #74787E; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; height: 100%; line-height: 1.4; margin: 0; width: 100% !important;" bgcolor="#F2F4F6"><style type="text/css">
-// body {
-// width: 100% !important; height: 100%; margin: 0; line-height: 1.4; background-color: #F2F4F6; color: #74787E; -webkit-text-size-adjust: none;
-// }
-// @media only screen and (max-width: 600px) {
-// .email-body_inner {
-// width: 100% !important;
-// }
-// .email-footer {
-// width: 100% !important;
-// }
-// }
-// @media only screen and (max-width: 500px) {
-// .button {
-// width: 100% !important;
-// }
-// }
-// </style>
-// <table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0; padding: 0; width: 100%;" bgcolor="#F2F4F6">
-// <tr>
-// <td align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
-// <table class="email-content" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0; padding: 0; width: 100%;">
-// <tr>
-//     <td class="email-masthead" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; padding: 25px 0; word-break: break-word;" align="center">
-//         <h2  class="email-masthead_name" style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 16px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
-// Comité de Vinculación Universidad Sociedad 
-// </h2>
-//     </td>
-// </tr>
+</head>
+<body style="-webkit-text-size-adjust: none; box-sizing: border-box; color: #74787E; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; height: 100%; line-height: 1.4; margin: 0; width: 100% !important;" bgcolor="#F2F4F6"><style type="text/css">
+body {
+width: 100% !important; height: 100%; margin: 0; line-height: 1.4; background-color: #F2F4F6; color: #74787E; -webkit-text-size-adjust: none;
+}
+@media only screen and (max-width: 600px) {
+.email-body_inner {
+width: 100% !important;
+}
+.email-footer {
+width: 100% !important;
+}
+}
+@media only screen and (max-width: 500px) {
+.button {
+width: 100% !important;
+}
+}
+</style>
+<table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0; padding: 0; width: 100%;" bgcolor="#F2F4F6">
+<tr>
+<td align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
+<table class="email-content" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0; padding: 0; width: 100%;">
+<tr>
+    <td class="email-masthead" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; padding: 25px 0; word-break: break-word;" align="center">
+        <h2  class="email-masthead_name" style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 16px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
+Comité de Vinculación Universidad Sociedad 
+</h2>
+    </td>
+</tr>
 
-// <tr>
-//     <td class="email-body" width="100%" cellpadding="0" cellspacing="0" style="-premailer-cellpadding: 0; -premailer-cellspacing: 0; border-bottom-color: #EDEFF2; border-bottom-style: solid; border-bottom-width: 1px; border-top-color: #EDEFF2; border-top-style: solid; border-top-width: 1px; box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0; padding: 0; width: 100%; word-break: break-word;" bgcolor="#FFFFFF">
-//         <table class="email-body_inner" align="center" width="800" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0 auto; padding: 0; width: 570px;" bgcolor="#FFFFFF">
-
-
-//             <tr>
-//                 <td class="content-cell" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; padding: 35px; word-break: break-word;">
-//                     <h4 style="box-sizing: border-box; color: #2F3133; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 19px; font-weight: bold; margin-top: 0;" align="left">Estimado: ' . $estudiante . ' </h4>
+<tr>
+    <td class="email-body" width="100%" cellpadding="0" cellspacing="0" style="-premailer-cellpadding: 0; -premailer-cellspacing: 0; border-bottom-color: #EDEFF2; border-bottom-style: solid; border-bottom-width: 1px; border-top-color: #EDEFF2; border-top-style: solid; border-top-width: 1px; box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0; padding: 0; width: 100%; word-break: break-word;" bgcolor="#FFFFFF">
+        <table class="email-body_inner" align="center" width="800" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0 auto; padding: 0; width: 570px;" bgcolor="#FFFFFF">
 
 
-//                     <table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 30px auto; padding: 0; text-align: center; width: 100%;">
-//                         <tr>
-//                             <td align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
+            <tr>
+                <td class="content-cell" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; padding: 35px; word-break: break-word;">
+                    <h4 style="box-sizing: border-box; color: #2F3133; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 19px; font-weight: bold; margin-top: 0;" align="left">Estimado: ' . $estudiante . ' </h4>
 
-//                                 <table width="100%" border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;">
-//                                     <tr>
-//                                         <td align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
-//                                             <table border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;">
-//                                                 <tr>
-//                                                     <td style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
 
-//                                                     </td>
-//                                                 </tr>
-//                                             </table>
-//                                         </td>
-//                                     </tr>
-//                                 </table>
-//                             </td>
-//                         </tr>
-//                     </table>
-//                     <p style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 16px; line-height: 1.5em; margin-top: 0;" align="left">Un placer saludarle, deseándole abundantes éxitos. Por este medio el comité de Vinculación Universidad-Sociedad del departamento de Informática-UNAH, le informa que Aprueba su Práctica Profesional Supervisada, ésta se concede en los términos siguientes:<br>
-// <br> 1.	Institución: ' . $empresa_prac . '
-// <br> 2.	Fecha de inicio: ' . $fecha_inicio_prac . '
-// <br> 3.	Fecha de Finalización: ' . $fecha_final_prac . '
-// <br> 4.	Dias: ' . $dias_prac . '
-// <br> 5.	Horario: ' . $horario_incio_prac . '
-// <br>
-// <br>Usted no puede terminar su práctica antes de esta fecha, ni realizar cambios ni tratos sin previa consulta al comité, de lo contrario, no será tomada como válida. En caso de requerir cambios deberá realizar la solicitud por escrito, presentando la solicitud al comité y enviando una copia digital a este correo. 
-// <br>
-// <br>El tiempo total de la práctica, se calculó con base al horario y se tomó en cuenta los feriados oficiales, haciendo un total de 800 horas. 
-// <br>
-// <br>Cualquier cambio, o situación anómala, debe ser reportado lo más pronto posible al Comité de Vinculación del departamento de Informática mediante carta y una copia al correo electrónico. Asimismo, en caso requerir trabajar horas extras debe reportarlas con anticipación a través de un correo o nota de su jefe inmediato. 
-// </p>
-//                     <p style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 16px; line-height: 1.5em; margin-top: 0;" align="left">
-//                         <br />Coordinación del Departamento de Informática</p>
+                    <table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 30px auto; padding: 0; text-align: center; width: 100%;">
+                        <tr>
+                            <td align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
 
-//                     <table class="body-sub" style="border-top-color: #EDEFF2; border-top-style: solid; border-top-width: 1px; box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin-top: 25px; padding-top: 25px;">
-//                         <tr>
-//                             <td style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
+                                <table width="100%" border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;">
+                                    <tr>
+                                        <td align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
+                                            <table border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;">
+                                                <tr>
+                                                    <td style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
 
-//                             </td>
-//                         </tr>
-//                     </table>
-//                 </td>
-//             </tr>
-//         </table>
-//     </td>
-// </tr>
-// <tr>
-//     <td style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
-//         <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0 auto; padding: 0; text-align: center; width: 570px;">
-//             <tr>
-//                 <td class="content-cell" align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; padding: 35px; word-break: break-word;">
-//                     <p class="sub align-center" style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 12px; line-height: 1.5em; margin-top: 0;" align="center">Facultad de Ciencias Económicas <br/>Departamento de Informatica Administrativa <br/>Comité de Vinculación Universidad Sociedad</p>
-//                     <p class="sub align-center" style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 12px; line-height: 1.5em; margin-top: 0;" align="center">
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <p style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 16px; line-height: 1.5em; margin-top: 0;" align="left">Un placer saludarle, deseándole abundantes éxitos. Por este medio el comité de Vinculación Universidad-Sociedad del departamento de Informática-UNAH, le informa que Aprueba su Práctica Profesional Supervisada, ésta se concede en los términos siguientes:<br>
+<br> 1.	Institución: ' . $empresa_prac . '
+<br> 2.	Fecha de inicio: ' . $fecha_inicio_prac . '
+<br> 3.	Fecha de Finalización: ' . $fecha_final_prac . '
+<br> 4.	Dias: ' . $dias_prac . '
+<br> 5.	Horario: ' . $horario_incio_prac . '
+<br>
+<br>Usted no puede terminar su práctica antes de esta fecha, ni realizar cambios ni tratos sin previa consulta al comité, de lo contrario, no será tomada como válida. En caso de requerir cambios deberá realizar la solicitud por escrito, presentando la solicitud al comité y enviando una copia digital a este correo. 
+<br>
+<br>El tiempo total de la práctica, se calculó con base al horario y se tomó en cuenta los feriados oficiales, haciendo un total de 800 horas. 
+<br>
+<br>Cualquier cambio, o situación anómala, debe ser reportado lo más pronto posible al Comité de Vinculación del departamento de Informática mediante carta y una copia al correo electrónico. Asimismo, en caso requerir trabajar horas extras debe reportarlas con anticipación a través de un correo o nota de su jefe inmediato. 
+</p>
+                    <p style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 16px; line-height: 1.5em; margin-top: 0;" align="left">
+                        <br />Coordinación del Departamento de Informática</p>
 
-//                     </p>
-//                 </td>
-//             </tr>
-//         </table>
-//     </td>
-// </tr>
-// </table>
-// </td>
-// </tr>
-// </table>
-// </body>
-// </html>
-// ';
+                    <table class="body-sub" style="border-top-color: #EDEFF2; border-top-style: solid; border-top-width: 1px; box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin-top: 25px; padding-top: 25px;">
+                        <tr>
+                            <td style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
+
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
+<tr>
+    <td style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; word-break: break-word;">
+        <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; margin: 0 auto; padding: 0; text-align: center; width: 570px;">
+            <tr>
+                <td class="content-cell" align="center" style="box-sizing: border-box; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; padding: 35px; word-break: break-word;">
+                    <p class="sub align-center" style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 12px; line-height: 1.5em; margin-top: 0;" align="center">Facultad de Ciencias Económicas <br/>Departamento de Informatica Administrativa <br/>Comité de Vinculación Universidad Sociedad</p>
+                    <p class="sub align-center" style="box-sizing: border-box; color: #000000; font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 12px; line-height: 1.5em; margin-top: 0;" align="center">
+
+                    </p>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
+';
 
 $correo_estud = "luis_davipacheco@hotmail.com";
-$nombre_estud = "Luis David";
-$prueba_cuerpo = "prueba cuerpo";
+$nombre_estud = "Luis David Pacheco Pineda";
 
-$correo->correo_aprobacion_prac($prueba_cuerpo, $asunto_estudiante, $correo_estud, $nombre_estud);
+
+$correo->correo_aprobacion_prac($cuerpo_aproba, $asunto_estudiante_aproba, $correo_estud, $nombre_estud);
 
 if ($consulta === 1) {
     bitacora::evento_bitacora($id_objeto, $_SESSION['id_usuario'], 'APROBÓ', 'UN NUEVO PRACTICANTE');
