a6577c109b8fcecbce916c521f028ee10e8a2d1d
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-08-24 14:51:18 -0600

    cam

--- clases/conexion2.php
+++ clases/conexion2.php
@@ -1,11 +1,11 @@
 <?php
 class conexion2
 {
-    private const DBHOST = '167.114.169.207';
-    private const DBUSER = 'informat_desarrollo';
-    private const DBPASS = '!fuRCr3XR-tzv';
-    private const DBNAME = 'informat_desarrollo_automatizacion';
-    private $dsn = 'mysql:host=' . self::DBHOST . ';dbname=' . self::DBNAME .'';
+    public const DBHOST = '167.114.169.207';
+    public const DBUSER = 'informat_desarrollo';
+    public const DBPASS = '!fuRCr3XR-tzv';
+    public const DBNAME = 'informat_desarrollo_automatizacion';
+    public $dsn = 'mysql:host=' . self::DBHOST . ';dbname=' . self::DBNAME .'';
     protected $conn = null;
 
     function __construct()
@@ -13,7 +13,7 @@ class conexion2
         try {
             $this->conn = new PDO($this->dsn, self::DBUSER, self::DBPASS);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            echo 'conecta';
+            // echo 'conecta';
         } catch (PDOException $e) {
             die('Error' . $e->getMessage());
         }
