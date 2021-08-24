e0551c6d9713ff322021df42fc34eee0e334d344
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-08-24 15:00:46 -0600

    cam
    ghmghmhmh

--- clases/conexion2.php
+++ clases/conexion2.php
@@ -5,7 +5,7 @@ class conexion2
     public const DBUSER = 'informat_desarrollo';
     public const DBPASS = '!fuRCr3XR-tzv';
     public const DBNAME = 'informat_desarrollo_automatizacion';
-    public $dsn = 'mysql:host=' . self::DBHOST . ';dbname=' . self::DBNAME .'';
+    public $dsn = ('mysql:host=' . self::DBHOST . ';dbname=' . self::DBNAME);
     protected $conn = null;
 
     function __construct()
