de4b118df3bca34bf37b4ab5c54ea88e856750b6
fany121 <61065845+fany121@users.noreply.github.com>
2021-08-30 13:48:57 -0600

    repotre excel

--- Controlador/reporte_carga_docente_excel.php
+++ Controlador/reporte_carga_docente_excel.php
@@ -1,45 +1,51 @@
 
 <?php
 //export.php  
-$servidor = "den1.mysql3.gear.host";
-$usuario = "automatizacion2";
-$password = "Be50i?1!guh3";
-$base = "automatizacion2";
+$servidor = "167.114.169.207";
+$usuario = "informat_desarrollo";
+$password = "!fuRCr3XR-tz";
+$base = "informat_desarrollo_automatizacion";
 
 
 $connect = mysqli_connect($servidor, $usuario, $password, $base);
 $output = '';
 if (isset($_POST["export"])) {
-    $query = "call sel_reporte_docente()";
+    $query = "call sel_reporte_preliminar_carga()";
     $result = mysqli_query($connect, $query);
     if (mysqli_num_rows($result) > 0) {
         $output .= '
         
-   <table class="table" bordered="1">  
+   <table class="table table-bordered table-striped" bordered="1" style="color: black;">  
      <h1>Reporte Carga Academica Docente </h1>
                     <tr>  
-                        <th>CODIGO ASIGNATURA</th>  
-                        <th>ASIGNATURA</th>  
-                        <th>SECCION</th>
+                        <th>#</th>
+                        <th>COD</th>  
+                        <th>Asignatura</th>  
+                        <th>Seccion</th>
                         <th>HI</th>
                         <th>HF</th>
-                        <th>EDIFICIO</th>
-                        <th>DIAS</th>
-                        <th>AULA</th>
-                        <th>N. ALUMNOS</th>
+                        <th>Dias</th>
+                        <th>Aula</th>
+                        <th>Edificio</th>
+                        <th>N</th>
+                        <th>Profesor</th>
+                        <th>Cupos</th>
                     </tr>
   ';
         while ($row = mysqli_fetch_array($result)) {
             $output .= '
     <tr>                         
+                            <td>' . $row["numero"] . '</td>  
                             <td>' . $row["codigo"] . '</td>  
                             <td>' . $row["asignatura"] . '</td>  
-                            <td>' . $row["seccion"] . '</td>  
-                            <td>' . $row["hra_inicio"] . '</td>
-                            <td>' . $row["hra_final"] . '</td>
-                            <td>' . $row["edificio"] . '</td> 
+                            <td>' . $row["seccion"] . '</td>
+                            <td>' . $row["hra_inicial"] . '</td>
+                            <td>' . $row["hra_final"] . '</td> 
                             <td>' . $row["dia"] . '</td> 
                             <td>' . $row["aula"] . '</td> 
+                            <td>' . $row["edificio"] . '</td>
+                            <td>' . $row["num_empleado"] . '</td>
+                            <td>' . $row["docente"] . '</td>
                             <td>' . $row["num_alumnos"] . '</td>
                     </tr>
    ';
