00387f7e8a22939814cb84b5153f07b7bd3b94ab
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-08 01:02:27 -0600

    alertas del login

---
+++ vistas/pagina_inicio_login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+</head>
+
+<body>
+    <div>
+
+
+
+
+        <!-- nav -->
+
+
+
+
+
+        <!-- Main Sidebar Container -->
+
+
+
+
+    </div>
+
+
+
+
+</body>
+
+</html>
