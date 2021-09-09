4c078baf0b42ee767c4646c0431b78f29725435d
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-09-09 12:36:16 -0600

    invs

--- Controlador/anular_salida_controlador.php
+++ Controlador/anular_salida_controlador.php
@@ -1,23 +1,23 @@
 
-<?php 
+<?php
 
 ob_start();
 session_start();
 
-require_once ('../clases/Conexion.php');
-require_once ('../clases/funcion_permisos.php');
-require_once ('../clases/funcion_bitacora.php');
+require_once('../clases/Conexion.php');
+require_once('../clases/funcion_permisos.php');
+require_once('../clases/funcion_bitacora.php');
 
 
-$Id_objeto=208;
+$Id_objeto = 208;
 
 // if (isset($_GET['motivo'])) {
-   
+
 //    $motivo = $_GET['motivo'];
 // }
 
 // if (isset($_GET['estado'])) {
-   
+
 //    $estado = $_GET['estado'];
 // }
 
@@ -28,36 +28,19 @@ $Id_objeto=208;
 
 
 
-if (permisos::permiso_eliminar($Id_objeto)=='0') {
+$motivo = 1;
+$estado = 'ANULADO';
+$inventario = 'INV-5';
 
-  echo '<script type="text/javascript">
-                              swal({
-                                   title:"",
-                                   text:"Lo sentimos no tiene permiso para eliminar",
-                                   type: "error",
-                                   showConfirmButton: false,
-                                   timer: 3000
-                                });
-                                $(".FormularioAjax")[0].reset();
-                                               window.location = "../vistas/gestion_salida_vista.php";
-
-                            </script>';
-}else{
-
-   $motivo = 1;
-   $estado ='ANULADO';
-   $inventario = 'INV-5';
-
- if ($estado==='PROCESADO'){
+if ($estado === 'PROCESADO') {
    $sql = "call proc_anular_salidas('$motivo','$inventario')";
    $resultado = $mysqli->query($sql);
-    if($resultado == TRUE){
-       bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'],'ANULÓ' , ' LA SALIDA DEL PRODUCTO CON NO.INVENTARIO  '. $inventario.' ');
- 
-                            
-  
-   echo
-                             '<script type="text/javascript">
+   if ($resultado == TRUE) {
+      bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'], 'ANULÓ', ' LA SALIDA DEL PRODUCTO CON NO.INVENTARIO  ' . $inventario . ' ');
+
+
+
+      echo '<script type="text/javascript">
                                swal({
                                     title:"",
                                     text:"Los datos se anularon correctamente",
@@ -69,11 +52,9 @@ if (permisos::permiso_eliminar($Id_objeto)=='0') {
                                  $(".FormularioAjax")[0].reset();
                 window.location = "../vistas/gestion_salida_vista.php";
  
-                             </script>'
-                             ;                      
- 
-                         }else{
-                            echo '<script type="text/javascript">
+                             </script>';
+   } else {
+      echo '<script type="text/javascript">
                                      swal({
                                         title:"",
                                         text:"El registro no puede ser eliminado",
@@ -83,18 +64,15 @@ if (permisos::permiso_eliminar($Id_objeto)=='0') {
                                      });
                                       $(".FormularioAjax")[0].reset();
                                  </script>';
-                         }
-
-
-}elseif ($estado='ANULADO')
-{
+   }
+} else if ($estado == 'ANULADO') {
    $sql = "call proc_anular_salida2('$motivo','$inventario')";
    $resultado = $mysqli->query($sql);
-    if($resultado == TRUE){
-       bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'],'RESTAURÓ' , 'LA SALIDA DEL PRODUCTO CON NO.INVENTARIO  '. $inventario.' ');
- 
-      
-                            echo '<script type="text/javascript">
+   if ($resultado == TRUE) {
+      bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'], 'RESTAURÓ', 'LA SALIDA DEL PRODUCTO CON NO.INVENTARIO  ' . $inventario . ' ');
+
+
+      echo '<script type="text/javascript">
                                swal({
                                     title:"",
                                     text:"Los datos se restauraron correctamente",
@@ -105,11 +83,9 @@ if (permisos::permiso_eliminar($Id_objeto)=='0') {
                                  $(".FormularioAjax")[0].reset();
                 window.location = "../vistas/gestion_salida_vista.php";
  
-                             </script>'
-                             ;                      
- 
-                         }else{
-                            echo '<script type="text/javascript">
+                             </script>';
+   } else {
+      echo '<script type="text/javascript">
                                      swal({
                                         title:"",
                                         text:"El registro no puede ser eliminado",
@@ -119,12 +95,8 @@ if (permisos::permiso_eliminar($Id_objeto)=='0') {
                                      });
                                       $(".FormularioAjax")[0].reset();
                                  </script>';
-                         }
-
-
-
-  
+   }
 }
-}
+
 ob_end_flush();
 ?>
