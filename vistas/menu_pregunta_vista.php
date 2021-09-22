b8a609a983124b65a1f1ebee61493e28a0966e20
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-21 20:23:40 -0600

    s

--- vistas/menu_pregunta_vista.php
+++ vistas/menu_pregunta_vista.php
@@ -82,7 +82,7 @@ $_SESSION['gestion_pregunta_menu']="No
   <div class="icon">
     <i class="far fa-plus-square"></i>
   </div>
-  <a href="../vistas/crear_pregunta_vista.php" class="small-box-footer">
+  <a href="../vistas/gestion_preguntas_vista.php" class="small-box-footer">
    Ir <i class="fas fa-arrow-circle-right"></i>
   </a>
 </div>
