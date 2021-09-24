3717b9530f8d84793891f66a27b611a0bef7acd6
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-24 16:18:15 -0600

    b

--- vistas/recuperar_x_correo_vista.php
+++ vistas/recuperar_x_correo_vista.php
@@ -52,7 +52,7 @@ if (isset($_REQUEST['idusuario'])) {
         <form action="../Controlador/verificar_correo_usuario_controlador.php?idusuario=<?php echo $id_usuario ?>" method="post">
 
           <div class="input-group mb-3">
-            <input type="email" class="form-control" id="correoclave" name="correoclave" value="" onkeypress="return correovalido('#correoclave')" onkeyup="Espacio(this, event)" maxlength="50" onkeypress="return comprobar(this.value, event, this.id)" placeholder="Correo Electronico" required="required">
+            <input type="email" class="form-control" id="correoclave" name="correoclave"  onkeyup="Espacio(this, event)" maxlength="50" onkeypress="return comprobar(this.value, event, this.id)" placeholder="Correo Electronico" required="required">
 
             <div class="input-group-append">
               <div class="input-group-text">
