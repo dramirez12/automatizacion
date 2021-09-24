53bf953cda326cb090160bd2759fefa43a5b43c4
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-24 14:39:16 -0600

    s

--- Controlador/actualizar_clave_x_pregunta_controlador.php
+++ Controlador/actualizar_clave_x_pregunta_controlador.php
@@ -37,15 +37,27 @@ if ($_POST) {
 ?>
 
 
-<html lang="es">
+<html lang="en">
 
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- <link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/bootstrap-theme.css" rel="stylesheet"> -->
-		<!-- <script src="js/jquery-3.3.1.min.js"></script> -->
-		<!-- <script src="js/bootstrap.min.js"></script> -->
+	<!-- <script src="js/jquery-3.3.1.min.js"></script> -->
+	<!-- <script src="js/bootstrap.min.js"></script> -->
+	<link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- icheck bootstrap -->
+	<link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="../dist/css/adminlte.min.css">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
 <body>
