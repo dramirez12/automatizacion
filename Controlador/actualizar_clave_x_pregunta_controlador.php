10f15838b1a5f137b6ec7b1c8abb5d347d121c54
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-24 13:51:35 -0600

    n

--- Controlador/actualizar_clave_x_pregunta_controlador.php
+++ Controlador/actualizar_clave_x_pregunta_controlador.php
@@ -40,11 +40,11 @@ if ($_POST) {
 
 <head>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/bootstrap-theme.css" rel="stylesheet">
-	<script src="js/jquery-3.3.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+		<!-- <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-theme.css" rel="stylesheet">
+		<script src="js/jquery-3.3.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script> -->
 </head>
 
 <body>
