a1950772e49e0fa683c27bc08362fc744589e2d2
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-24 13:06:46 -0600

    m

--- vistas/cambiar_clave_x_pregunta_vista.php
+++ vistas/cambiar_clave_x_pregunta_vista.php
@@ -127,12 +127,6 @@ if (isset($_REQUEST['msj'])) {
   </div>
   <!-- /.login-box -->
 
-  <!-- jQuery -->
-  <script src="../plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="../dist/js/adminlte.min.js"></script>
 
 
   <script>
