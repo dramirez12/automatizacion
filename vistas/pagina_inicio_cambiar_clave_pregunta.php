fa5cd3d036f88a37c244a3b8fcdcb07b105baaa3
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-24 14:34:28 -0600

    d

--- vistas/pagina_inicio_cambiar_clave_pregunta.php
+++ vistas/pagina_inicio_cambiar_clave_pregunta.php
@@ -7,7 +7,7 @@
 
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
