3f48f90742735f7020cb8a7cddf3ec26ae70d0fa
fany121 <61065845+fany121@users.noreply.github.com>
2021-09-29 15:18:14 -0600

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "conexion a producc"""""""""""""""""""
    This reverts commit 9786531652f4cb5f034f6bb59680bfcd36ff8432.

--- clases/global.php
+++ clases/global.php
@@ -3,17 +3,16 @@
 define("DB_HOST","167.114.169.207");
 
 //Nombre de la base de datos
-define("DB_NAME", "informat_automatizacion");
+define("DB_NAME", "informat_desarrollo_automatizacion");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "informat_informaticaunah2");
+define("DB_USERNAME", "informat_desarrollo");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "dF%&(M6tJL?P");
+define("DB_PASSWORD", "!fuRCr3XR-tz");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
 
 //Definimos una constante como nombre del proyecto
 define("PRO_NOMBRE","ITVentas");
-
