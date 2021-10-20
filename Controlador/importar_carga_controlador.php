e70b2ba5f564d1c255ae6964efb79aeaf806934b
fany121 <61065845+fany121@users.noreply.github.com>
2021-10-20 12:50:55 -0600

    Revert "s"
    This reverts commit f3c6fba3c4b2a928af7409ef2611cf112ddca3af.

--- Controlador/importar_carga_controlador.php
+++ Controlador/importar_carga_controlador.php
@@ -9,25 +9,24 @@ if (isset($_GET['op'])) {
     switch ($_GET['op']) {
 
         case 'cargarExcel':
-            try {
 
-                if (is_array($_FILES["archivoExcel"]) && count($_FILES["archivoExcel"]) > 0) {
+            if (is_array($_FILES["archivoExcel"]) && count($_FILES["archivoExcel"]) > 0) {
 
-                    //SE LLAMA A LA LIBRERIA
-                    require_once('../PHPExcel/Classes/PHPExcel.php');
+                //SE LLAMA A LA LIBRERIA
+                require_once('../PHPExcel/Classes/PHPExcel.php');
 
-                    $tmpfname = $_FILES['archivoExcel']['tmp_name'];
+                $tmpfname = $_FILES['archivoExcel']['tmp_name'];
 
-                    //CREAR EL EXCEL PARA LUEGO LEERLO
-                    $leer_excel = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+                //CREAR EL EXCEL PARA LUEGO LEERLO
+                $leer_excel = PHPExcel_IOFactory::createReaderForFile($tmpfname);
 
-                    //CARGAR NUESTRO EXCELL
-                    $excel_obj = $leer_excel->load($tmpfname);
+                //CARGAR NUESTRO EXCELL
+                $excel_obj = $leer_excel->load($tmpfname);
 
-                    //CARGAR EN QUE HOJA TRABAJAREMOS DEL EXCEL
-                    $hoja = $excel_obj->getSheet(0);
-                    $filas = $hoja->getHighestRow();
-                    echo "<table id='tabla_detalle' class='table' style='width: 100%; table-layout:fixed'>
+                //CARGAR EN QUE HOJA TRABAJAREMOS DEL EXCEL
+                $hoja = $excel_obj->getSheet(0);
+                $filas = $hoja->getHighestRow();
+                echo "<table id='tabla_detalle' class='table' style='width: 100%; table-layout:fixed'>
                 <thead>
                     <tr bgcolor ='black' style='color: #FFFFFF'> 
                         <td>Control</td>
@@ -47,44 +46,43 @@ if (isset($_GET['op'])) {
                 ";
 
 
-                    for ($row = 2; $row <= $filas; $row++) {
+                for ($row = 2; $row <= $filas; $row++) {
 
-                        $control = $hoja->getCell('A' . $row);
-                        $cod_asig = $hoja->getCell('B' . $row);
-                        $asignatura = $hoja->getCell('C' . $row);
-                        $seccion = $hoja->getCell('D' . $row);
-                        $hora_inicio = $hoja->getCell('E' . $row);
-                        $hora_final = $hoja->getCell('F' . $row);
-                        $dias = $hoja->getCell('G' . $row);
-                        $aula = $hoja->getCell('H' . $row);
-                        $profesor = $hoja->getCell('K' . $row);
-                        $matriculados = $hoja->getCell('L' . $row);
+                    $control = $hoja->getCell('A'.$row);
+                    $cod_asig = $hoja->getCell('B'.$row);
+                    $asignatura = $hoja->getCell('C'.$row);
+                    $seccion = $hoja->getCell('D'.$row);
+                    $hora_inicio = $hoja->getCell('E'.$row);
+                    $hora_final = $hoja->getCell('F'.$row);
+                    $dias = $hoja->getCell('G'.$row);
+                    $aula = $hoja->getCell('H'.$row);
+                    $profesor = $hoja->getCell('K'.$row);
+                    $matriculados = $hoja->getCell('L'.$row);
 
-                        if ($control == "") {
-                        } else {
-                            echo "<tr>";
-                            echo "<td>" . $control . "</td>";
-                            echo "<td>" . $cod_asig . "</td>";
-                            echo "<td>" . $asignatura . "</td>";
-                            echo "<td>" . $seccion . "</td>";
-                            echo "<td>" . $hora_inicio . "</td>";
-                            echo "<td>" . $hora_final . "</td>";
-                            echo "<td>" . $dias . "</td>";
-                            echo "<td>" . $aula . "</td>";
-                            echo "<td>" . $profesor . "</td>";
+                    if ($control =="") {
+                        
+                    }else{
+                        echo "<tr>";
+                        echo "<td>" . $control . "</td>";
+                        echo "<td>" . $cod_asig . "</td>";
+                        echo "<td>" . $asignatura . "</td>";
+                        echo "<td>" . $seccion . "</td>";
+                        echo "<td>" . $hora_inicio . "</td>";
+                        echo "<td>" . $hora_final . "</td>";
+                        echo "<td>" . $dias . "</td>";
+                        echo "<td>" . $aula . "</td>";
+                        echo "<td>" . $profesor . "</td>";
                             echo "<td>" . $matriculados . "</td>";
-                            echo "</tr>";
-                        }
-                    }
+                        echo "</tr>";
 
-                    echo "</tbody></table>";
-                } else {
-                    return 0;
+                    }
                 }
-            } catch (\Throwable $th) {
-                echo $th;
+
+                echo "</tbody></table>";
+                
+            } else {
+                return 0;
             }
-           
 
 
 
