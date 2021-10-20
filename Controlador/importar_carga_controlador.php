a5595b9ad78399337ad32bf32a4d01b25b145c11
fany121 <61065845+fany121@users.noreply.github.com>
2021-10-20 11:10:13 -0600

    Revert "s"
    This reverts commit d42d5a784b49e9c73323e6e95cbecf2e55ba9fa1.

--- Controlador/importar_carga_controlador.php
+++ Controlador/importar_carga_controlador.php
@@ -90,7 +90,86 @@ if (isset($_GET['op'])) {
             //aqui va la preliminar
 
 
-      
+        case 'cargarExcel_preliminar':
+
+            if (is_array($_FILES["archivo_excel_preliminar"]) && count($_FILES["archivo_excel_preliminar"]) > 0
+            ) {
+
+                //SE LLAMA A LA LIBRERIA
+                require_once('../PHPExcel/Classes/PHPExcel.php');
+
+                $tmpfname = $_FILES['archivo_excel_preliminar']['tmp_name'];
+
+                //CREAR EL EXCEL PARA LUEGO LEERLO
+                $leer_excel = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+
+                //CARGAR NUESTRO EXCELL
+                $excel_obj = $leer_excel->load($tmpfname);
+
+                //CARGAR EN QUE HOJA TRABAJAREMOS DEL EXCEL
+                $hoja = $excel_obj->getSheet(0);
+                $filas = $hoja->getHighestRow();
+                echo "<table id='tabla_detalle_preliminar' class='table' style='width: 100%; table-layout:fixed'>
+                <thead>
+                    <tr bgcolor ='black' style='color: #FFFFFF'> 
+                       
+                        <td>Cod</td>
+                        <td>Asignatura</td>
+                        <td>Seccion</td>
+                        <td>Hra Inicio</td>
+                        <td>Hra Final</td>
+                        <td>Dias</td>
+                        <td>Aulas</td>
+                        <td>Profesor</td>
+                        <td>Matriculados</td>
+                       
+            
+                    </tr>
+                </thead>
+                <tbody id='tbody_tabla_detalle_preliminar'>
+                ";
+
+
+                for ($row = 5;
+                    $row <= $filas;
+                    $row++
+                ) {
+
+                    $cod_asig = $hoja->getCell('A' . $row);
+                    $asignatura = $hoja->getCell('B' . $row);
+                    $seccion = $hoja->getCell('C' . $row);
+                    $hora_inicio = $hoja->getCell('E' . $row);
+                    $hora_final = $hoja->getCell('F' . $row);
+                    $dias = $hoja->getCell('G' . $row);
+                    $aula = $hoja->getCell('H' . $row);
+                    $profesor = $hoja->getCell('J' . $row);
+                    $matriculados = $hoja->getCell('K' . $row);
+                  //  $control = $hoja->getCell('M' . $row);
+
+                    if ($cod_asig == "") {
+                    } else {
+                        echo "<tr>";
+                        echo "<td>" . $cod_asig . "</td>";
+                        echo "<td>" . $asignatura . "</td>";
+                        echo "<td>" . $seccion . "</td>";
+                        echo "<td>" . $hora_inicio . "</td>";
+                        echo "<td>" . $hora_final . "</td>";
+                        echo "<td>" . $dias . "</td>";
+                        echo "<td>" . $aula . "</td>";
+                        echo "<td>" . $profesor . "</td>";
+                        echo "<td>" . $matriculados . "</td>";
+                        echo "</tr>";
+                    }
+                }
+
+                echo "</tbody></table>";
+            } else {
+                return 0;
+            }
+
+
+
+            break;
     }
 };
 ?>
