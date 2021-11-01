82754e0dec35b8ebb5f0bd379c69cfc9e6f14935
EdAl2019 <55669709+EdAl2019@users.noreply.github.com>
2021-11-01 11:11:03 -0600

    Cambios en el modulo VOAE
    Cambios por parte de Ever Palada

--- Controlador/registro_actividad_externa_controlador.php
+++ Controlador/registro_actividad_externa_controlador.php
@@ -10,7 +10,7 @@ require_once ('../clases/funcion_visualizar.php');
 require_once ('../clases/funcion_bitacora.php');
 
 $externa=new Externa();
-$Id_objeto=237; 
+$Id_objeto=294; 
 
 if (permisos::permiso_modificar($Id_objeto)==0)
   {
