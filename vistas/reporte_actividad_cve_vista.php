aa3ec9b65ab560a6a8562b13ad6ddba5fb56fdab
EdAl2019 <55669709+EdAl2019@users.noreply.github.com>
2021-11-01 11:46:49 -0600

    Cambios en el modulo VOAE v.2
    Cambios por parte de Ever Palada

--- vistas/reporte_actividad_cve_vista.php
+++ vistas/reporte_actividad_cve_vista.php
@@ -10,7 +10,7 @@ require_once('../clases/funcion_bitacora.php');
 require_once('../clases/funcion_visualizar.php');
 require_once('../clases/funcion_permisos.php');
 
-$Id_objeto = 236;
+$Id_objeto = 295;
 $hoy = date("y-m-d");
 
 
