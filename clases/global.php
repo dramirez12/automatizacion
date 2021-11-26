889e8bc57fd46e6bc1df36d31dc507f468d71187
EdAl2019 <55669709+EdAl2019@users.noreply.github.com>
2021-11-25 21:23:29 -0600

    Cambios global conexion

--- clases/global.php
+++ clases/global.php
@@ -3,16 +3,16 @@
 define("DB_HOST","51.222.86.251");
 
 //Nombre de la base de datos
-define("DB_NAME", "");
+define("DB_NAME", "informat_desarrollo_automatizacion");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "");
+define("DB_USERNAME", "informat_desarrollo");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", '');
+define("DB_PASSWORD", "^Kwd{PE^(L&#");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
 
 //Definimos una constante como nombre del proyecto
-
+define("PRO_NOMBRE","ITVentas");
